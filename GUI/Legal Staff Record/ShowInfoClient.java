510d71dbad8bb6933a1c2153db33fa4512608608
skouva <31855632+skouva@users.noreply.github.com>
2018-04-26 18:40:59 +0300

    Update ShowInfoClient.java

--- GUI/Legal Staff Record/ShowInfoClient.java
+++ GUI/Legal Staff Record/ShowInfoClient.java
@@ -62,7 +62,7 @@ public class ShowInfoClient {
 	
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 470);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblShowClientsInfo = new JLabel("Show Client's Info");
