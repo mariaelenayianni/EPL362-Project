e3b4dd324f77daac553db3edf9f486d73e736680
skouva <31855632+skouva@users.noreply.github.com>
2018-04-26 18:11:40 +0300

    Update DisagreementsUpload.java

--- GUI/Legal Staff Record/DisagreementsUpload.java
+++ GUI/Legal Staff Record/DisagreementsUpload.java
@@ -11,16 +11,19 @@ import ws.MySQLAccess;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class DisagreementsUpload {
 
 	private JFrame frame;
 	private JTextField textField;
-	private JTextField textField_1;
 	private Statement statement = null;
-	
+	private ResultSet resultSet = null;
+
 	/**
 	 * Launch the application.
 	 */
@@ -47,13 +50,14 @@ public class DisagreementsUpload {
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	@SuppressWarnings("unchecked")
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 341, 303);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JLabel lblUploadDisagreements = new JLabel("Upload Disagreements");
+		JLabel lblUploadDisagreements = new JLabel("Insert Disagreements");
 		lblUploadDisagreements.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblUploadDisagreements.setBounds(12, 13, 264, 23);
 		frame.getContentPane().add(lblUploadDisagreements);
@@ -67,17 +71,38 @@ public class DisagreementsUpload {
 		textField.setBounds(118, 85, 116, 22);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblStrategyid = new JLabel("StrategyID:");
 		lblStrategyid.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblStrategyid.setBounds(12, 146, 82, 23);
 		frame.getContentPane().add(lblStrategyid);
 
-		textField_1 = new JTextField();
-		textField_1.setBounds(118, 147, 116, 22);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-	
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Select S_ID"}));
+
+		MySQLAccess sqlcon = new MySQLAccess();
+		try {
+			statement = sqlcon.readDataBase();
+
+			resultSet = statement.executeQuery("select * from strategy");
+			while (resultSet.next()) {
+				String pat = resultSet.getString("strategyID");
+				comboBox.addItem(pat);
+			}
+		//	comboBox.setSelectedIndex(int);
+
+			comboBox.setSelectedItem("Select StrategyID");
+
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} finally {
+			sqlcon.close();
+		}
+
+		comboBox.setBounds(126, 147, 108, 22);
+		frame.getContentPane().add(comboBox);
+
 		JButton btnSumbit = new JButton("Submit");
 
 		btnSumbit.addActionListener(new ActionListener() {
@@ -86,22 +111,25 @@ public class DisagreementsUpload {
 
 				MySQLAccess sqlcon = new MySQLAccess();
 				try {
-					String strategyID = textField_1.getText();
+					int strategyID = comboBox.getSelectedIndex();
 					String clientID = textField.getText();
-		
-					statement=sqlcon.readDataBase();
-					statement.executeUpdate(
-							"UPDATE cases c, appointment a SET c.Disagree=1 WHERE c.appointmentID=a.appointmentID AND a.clientID='"
-									+ clientID + "' AND c.strategyID='" + strategyID + "'");
+					int c_ID = Integer.parseInt(clientID);
+					
+					Statement pst = sqlcon.readDataBase();
 
+					String query = "INSERT INTO disaggrements VALUES (default,'" + c_ID + "', '" + strategyID + "')";
+					pst.executeUpdate(query);
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
+				} finally {
+					sqlcon.close();
 				}
 			}
 		});
 		btnSumbit.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnSumbit.setBounds(177, 198, 121, 25);
 		frame.getContentPane().add(btnSumbit);
+
 	}
 }
