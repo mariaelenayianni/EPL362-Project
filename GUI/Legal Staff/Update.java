b9495721488b5db1e7aa2bdb11ee1a7c663f0eeb
mangel033 <marilenhhxd@gmail.com>
2018-04-27 01:15:26 +0300

    addUpdateUI

---
+++ GUI/Legal Staff/Update.java
@@ -0,0 +1,53 @@
+package wsclient;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Update {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Update window = new Update();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Update() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Arial Black", Font.BOLD, 14));
+		frame.setBounds(100, 100, 663, 460);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setVisible(true);
+		
+		JLabel lblNewLabel = new JLabel("Update");
+		lblNewLabel.setFont(new Font("Arial Black", Font.BOLD, 14));
+		lblNewLabel.setBounds(289, 11, 166, 25);
+		frame.getContentPane().add(lblNewLabel);
+	}
+
+}
