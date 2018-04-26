3ca02dc3c631f12fd8bf8e385b14e47d5a545798
mangel033 <marilenhhxd@gmail.com>
2018-04-27 01:11:44 +0300

    addGlobalVariablesForLegalStaffGUI

---
+++ GUI/Legal Staff/globals.java
@@ -0,0 +1,9 @@
+package wsclient;
+
+public class globals{
+	 public static int appointment;
+	 public static String action=new String();
+	 public static int appointment1;
+	 public static String lawyerID=new String();
+	 public static int clientID;
+}
