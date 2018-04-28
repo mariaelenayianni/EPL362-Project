1ef1b07352bb6cb702c62e1b5eb2193acf12df0a
epapad04 <epapad04@cs.ucy.ac.cy>
2018-04-28 20:40:17 +0300

    addReceptionistGlobals

---
+++ GUI/Receptionist/globals.java
@@ -0,0 +1,11 @@
+package wsclient;
+
+public class globals{
+	 public static int appointment;
+	 public static String action=new String();
+	 public static int appointment1;
+	 public static String lawyerID=new String();
+	 public static int clientID;
+	 public static java.sql.Date date;
+	 public static String time;
+}
